a9609e75f91b2485c76db0b751bddb8b064ffeb7
rishabmehra <rishabmehra10@gmail.com>
2019-01-28 17:05:06 +0530

    Rishab mehra (#2)
    * intial commit for rendering the default users while invoking the component
    
    * intial commiit added the css
    
    * Added the test cases and changes being made on the stylings
    
    * Final commit added proptypes and comments
    
    * final commit :- change the utils function name
    
    * Added some more testcases

--- src/components/__test__/Profiles.spec.js
+++ src/components/__test__/Profiles.spec.js
@@ -4,20 +4,38 @@ import  Profiles  from "../Profiles";
 
 describe("Profile", () => {
   let component;
+  let onDeleteAction = jest.fn();
   beforeEach(() => {
-    component = shallow(<Profiles key={`4`} profilename={`Evan Bold`} avatar={`test-url`} />);
+    component = shallow(<Profiles key={`4`} profilename={`Evan Bold`} avatar={`test-url`} onDeleteAction={onDeleteAction} />);
   });
 
-  it('should render profile component', () => {
+  it('should render the profile component', () => {
     expect(component.find('.profile__container')).toBeDefined();
   });
 
   it('should render the details of profile component', () => {
-    const profileName = component.find(".profile__container-name").first();
+    const profileName = component.find(".profile__container-name");
     expect(profileName.text()).toEqual("Evan Bold");
 
-    const imgUrl = component.find("img").first().prop('src');
+    const imgUrl = component.find("img").prop('src');
     expect(imgUrl).toEqual("test-url");
+
+    expect(component.find('.profile__container-delete')).toHaveLength(1);
   });
+  
+  it('should default props while invoking the profile component',() => {
+     const defaultComponent = shallow(<Profiles />);
+
+     expect(defaultComponent.find(".profile__container-name").text()).toEqual("John Mathew");
+     expect(defaultComponent.find("img").prop('src')).toEqual("https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg");
+  });
+
+  it('should call a click event once clicking on delete', () =>{
+    let mockFunc = jest.fn();
+    let defaultComponent = shallow(<Profiles onDeleteAction={mockFunc} />);
+
+    defaultComponent.find("a").simulate('click');
+    expect(mockFunc.mock.calls.length).toEqual(1);  
+  })
 
 });
